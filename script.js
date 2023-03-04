5bc7500cd3b7076705652a7dc9c3f3c816263764
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-03-04 23:56:34 +0600

    display all  cards section added

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log('js file added');
+const loadData = async dataLimit => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    spinnerToggle(true);
+    const res = await fetch(url);
+    const data = await res.json();
+    displayData(data.data.tools, dataLimit);
+    spinnerToggle(false);
+};
+
+const displayData = (cards, dataLimit) => {
+    const cardContainer = document.getElementById('cards-container');
+    cardContainer.innerHTML = '';
+    const seeMore = document.getElementById('see-more');
+
+    if (dataLimit && cards.length > 6) {
+        cards = cards.slice(0, 6);
+        seeMore.classList.remove('d-none');
+    } else {
+        seeMore.classList.add('d-none');
+    }
+
+    cards.forEach(card => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML = `
+            <div class="card p-3">
+                <img src="${card.image}" class="card-img-top" alt="card-image">
+                <div class="card-body">
+                    <h4 class ='card-title'>Features</h4>
+                    <ol>
+                        <li>${card.features[0]}</li>
+                        <li>${card.features[1]}</li>
+                        <li>${card.features[2]}</li>
+                    </ol>
+                    <hr>
+                    <h5>${card.name}</h5>
+                    <div class="d-flex justify-content-between">
+                        <div class="d-flex align-item-center gap-2 ">
+                            <i class=" py-2 fa-regular fa-calendar-days"></i>
+                            <P class="py-1" >${card.published_in}</P>
+                        </div>
+                        <div><i class="fs-3 fa-solid fa-circle-arrow-right" onclick="fetchCardDetails('${card.id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></div>
+                    </div> 
+                </div>
+            </div>
+        `;
+
+        cardContainer.appendChild(cardDiv);
+    });
+};
+
+
+
+loadData(6);
